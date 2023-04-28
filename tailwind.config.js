e8f7447b4cd5e16f1b7102195453b812d9a22859
Apheiro <eyentuyon@gmail.com>
2023-04-28 03:12:37 -0300

    Config tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ export default {
     "./src/app/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      '2xl': { 'min': '1535px' },
+      'xl': { 'min': '1279px' },
+      'lg': { 'min': '1023px' },
+      '2md': { 'min': '870px' },
+      'md': { 'min': '767px' },
+      '2sm': { 'min': '710px' },
+      'sm': { 'min': '639px' },
+    },
     extend: {
       colors: {
         db: "#121316",

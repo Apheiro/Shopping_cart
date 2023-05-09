43d2cb5ab613156b5274ceecf7234e994b264d75
Apheiro <eyentuyon@gmail.com>
2023-05-09 10:55:24 -0300

    Improve exports for components.

---
+++ src/Components/Core/Exports.tsx
@@ -0,0 +1,11 @@
+import Btn from "./Btn";
+import CategoryBtn from "./CategoryBtn";
+import Checkbox from "./Checkbox";
+import InfoCard from "./InfoCard";
+import Input from "./Input";
+import ProductCard from "./ProductCard";
+
+export {
+    Btn, CategoryBtn, Checkbox,
+    InfoCard, Input, ProductCard
+};
---
+++ src/Components/Exports.tsx
@@ -0,0 +1,21 @@
+import Categories from "./Categories";
+import Features from "./Features";
+import Start from "./Start";
+import Footer from "./Footer";
+import ImagesProduct from "./ImagesProduct";
+import Navbar from "./Navbar";
+import Pagination from "./Pagination";
+import Presentation from "./Presentation";
+import ProductAddPageCard from "./ProductAddPageCard";
+import ProductInCartCard from "./ProductInCartCard";
+import ProductResultCard from "./ProductResultCard";
+import SideMenu from "./SideMenu";
+import ProductsCarousel from "./ProductsCarousel";
+
+export {
+    Categories, Features, Start,
+    Footer, ImagesProduct, Navbar,
+    Pagination, Presentation, ProductAddPageCard,
+    ProductInCartCard, ProductResultCard,
+    SideMenu, ProductsCarousel
+};

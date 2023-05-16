85e4544563098f95ec683fb11a3588011acaef3c
Apheiro <eyentuyon@gmail.com>
2023-05-15 21:22:01 -0300

    Improve infoCard

--- src/Components/Core/InfoCard.tsx
+++ src/Components/Core/InfoCard.tsx
@@ -1,5 +1,5 @@
 import Btn from './Btn';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { IconChevronDown } from '@tabler/icons-react';
 
 interface Props {
@@ -11,8 +11,21 @@ interface Props {
 
 export default function InfoCard({ title, description, variant, customClass }: Props) {
     const [foldCard, setFoldCard] = useState<boolean>(false)
-    const titleStyle: string = 'flex items-center text-neutral-400 text-md 2sm:text-xl font-bold '
-    const descriptionStyle: string = 'text-neutral-300 text-sm'
+    const [seeMore, setSeeMore] = useState<boolean>(false)
+    const [itClamp, setItClamp] = useState<boolean>(false)
+
+    const titleStyle: string = 'flex items-center text-neutral-400 text-md 2sm:text-xl font-bold'
+    const descriptionStyle: string = `text-neutral-300 text-sm text-justify ${!seeMore && 'line-clamp-11'}`
+    const paraRef = useRef<HTMLParagraphElement>(null)
+
+    useEffect(() => {
+        if (variant === 'static') {
+            const height = paraRef.current?.clientHeight ?? 0
+            const scrollHeight = paraRef.current?.scrollHeight ?? 0
+            setItClamp(height < scrollHeight)
+        }
+    }, [description])
+
     return (
         <div className={`${customClass} bg-dbm h-fit rounded-lg p-3 w-full flex flex-col gap-2`} key={crypto.randomUUID()}>
             {
@@ -21,9 +34,10 @@ export default function InfoCard({ title, description, variant, customClass }: P
                     <div>
                         <h2 className={titleStyle}>{title}</h2>
                     </div>
-                    <p className={descriptionStyle}>
+                    <p ref={paraRef} className={descriptionStyle}>
                         {description}
                     </p>
+                    {itClamp && <Btn variant='base' onClick={() => setSeeMore(!seeMore)} classNameCustom={'!bg-dbm !p-0 w-fit justify-end self-end'}>  {seeMore ? 'Show less' : 'Show more'}</Btn>}
                 </>
             }
             {

af5852330a40235b0d0b4e1c692f1d2e303bdb5a
Apheiro <eyentuyon@gmail.com>
2023-05-15 21:24:40 -0300

    Add search features.

--- src/Pages/Search.tsx
+++ src/Pages/Search.tsx
@@ -1,26 +1,28 @@
-import { useState } from "react"
-import { useMediaQuery } from "@mantine/hooks"
-import { Btn } from "../Components/Core/Exports"
 import { ProductResultCard, Pagination, FilterForm, SortForm } from '../Components/Exports'
-import { IconFilter } from '@tabler/icons-react'
-import { useLoaderData, LoaderFunctionArgs } from "react-router-dom";
 import { getProductsList, ReqParams, SearchResult } from '../utils/productsRequests'
+import { useLoaderData, LoaderFunctionArgs } from "react-router-dom";
+import { IconFilter } from '@tabler/icons-react'
+import { Btn } from "../Components/Core/Exports"
+import { useMediaQuery } from "@mantine/hooks"
+import { useState } from "react"
 
 export async function loader(paramsLoader: LoaderFunctionArgs) {
     const actualURL = new URL(paramsLoader.request.url);
     let reqParams: ReqParams = {}
     actualURL.searchParams.forEach((value, key) => { reqParams[key] = value });
     const searchResult = await getProductsList(reqParams)
+    console.log('loader of search')
     return { searchResult, reqParams }
 }
 
 export default function Search() {
+    const { searchResult, reqParams } = useLoaderData() as { searchResult: SearchResult, reqParams: ReqParams };
     const [hiddeFilters, setHiddeFilters] = useState(false);
     const isMobile = useMediaQuery('(max-width: 768px)');
-    const { searchResult, reqParams } = useLoaderData() as { searchResult: SearchResult, reqParams: ReqParams };
+    const prodNotFound = searchResult.products.length === 0
 
     return (
-        <section className="min-h-screen flex justify-center items-center p-4 py-30">
+        <section className="min-h-screen flex justify-center p-4 py-30">
             <div className={`${isMobile ? 'flex flex-col' : 'grid'} ${hiddeFilters ? 'flex flex-col' : 'grid-cols-[auto_1fr] grid-rows-[auto_1fr]'} gap-4 w-full max-w-5xl`}>
                 <div className="bg-dbm h-fit p-3 rounded-lg flex flex-wrap gap-4 justify-center sm:justify-between items-center">
                     <div className="flex items-center gap-2">
@@ -30,9 +32,10 @@ export default function Search() {
                     <SortForm defaultValue={reqParams.sr} />
                 </div>
                 {!hiddeFilters && <FilterForm defaultValue={{ condition: reqParams.condition, min: reqParams.min, max: reqParams.max }} />}
-                <div className="flex flex-col gap-4 col-start-2 col-end-3">
+                <div className={`flex ${prodNotFound && 'justify-center items-center'} flex-col gap-4 col-start-2 col-end-3 h-full`}>
+                    {prodNotFound && <h1 className='text-2xl font-bold text-neutral-5'>Not products found</h1>}
                     {
-                        searchResult.products.map(({ image, condition, name, modelNumber, sku, salePrice }, index) =>
+                        searchResult.products.map(({ image, condition, name, modelNumber, sku, salePrice, regularPrice, orderable }, index) =>
                             <ProductResultCard
                                 key={`productSearchResult-${index}`}
                                 img={image}
@@ -41,6 +44,8 @@ export default function Search() {
                                 model={modelNumber}
                                 sku={sku}
                                 price={salePrice}
+                                oldPrice={regularPrice}
+                                orderable={orderable}
                             />
                         )
                     }

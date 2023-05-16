a185b9c42415d1210781fcc881ea0ac1dacbe462
Apheiro <eyentuyon@gmail.com>
2023-05-15 21:23:34 -0300

    Change router style.

--- src/Pages/RouteSwitch.tsx
+++ src/Pages/RouteSwitch.tsx
@@ -1,34 +1,38 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { Start } from "../Components/Exports"
-import Root from "./Root";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import Root, { loader as rootLoader, action as rootAction } from "./Root";
 import ErrorPage from "./ErrorPage";
+import Start from "./Start"
 import Search, { loader as searchLoader } from "./Search";
-import Product, { loader as productLoader } from "./Product";
+import Product, { loader as productLoader, action as productAction } from "./Product";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                index: true,
-                element: <Start />
-            },
-            {
-                path: "search",
-                element: <Search />,
-                loader: searchLoader
-            },
-            {
-                path: "product/:sku",
-                element: <Product />,
-                loader: productLoader
-            },
-        ]
-    },
 
-])
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route
+            path="/"
+            element={<Root />}
+            loader={rootLoader}
+            action={rootAction}
+            errorElement={<ErrorPage />}
+        >
+            <Route
+                index={true}
+                element={<Start />}
+            />
+            <Route
+                path="search"
+                element={<Search />}
+                loader={searchLoader}
+            />
+            <Route
+                path="product/:sku"
+                element={<Product />}
+                loader={productLoader}
+                action={productAction}
+            />
+        </Route>
+    )
+)
 
 export default function RouteSwitch() { return <RouterProvider router={router} /> }
 

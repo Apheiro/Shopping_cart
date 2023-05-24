62784a6a091f26846dfcb11059d8ba1d3bc566e9
Apheiro <eyentuyon@gmail.com>
2023-05-24 04:40:07 -0300

    Change Router.

--- src/Pages/RouteSwitch.tsx
+++ src/Pages/RouteSwitch.tsx
@@ -1,11 +1,17 @@
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import {
+    Route,
+    RouterProvider,
+    // createBrowserRouter,
+    createRoutesFromElements,
+    createHashRouter
+} from "react-router-dom";
 import Root, { loader as rootLoader, action as rootAction } from "./Root";
 import ErrorPage from "./ErrorPage";
 import Start from "./Start"
 import Search, { loader as searchLoader } from "./Search";
 import Product, { loader as productLoader, action as productAction } from "./Product";
 
-const router = createBrowserRouter(
+const router = createHashRouter(
     createRoutesFromElements(
         <Route
             path="/"
@@ -34,9 +40,12 @@ const router = createBrowserRouter(
             />
         </Route>
     ), {
-    basename: "/Shopping_cart",
-
+    // basename: "/Shopping_cart",
 }
 )
+
+// Im use hashRouter instead of browserRouter beacuse 
+// I cant configure the server side of gh pages for deriect 
+// all traffic to my app
 
 export default function RouteSwitch() { return <RouterProvider router={router} /> }
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
-  base: '/Shopping_cart',
+  base: '/Shopping_cart/',
   plugins: [
     react(),
     UnoCSS({
